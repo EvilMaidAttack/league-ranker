fa210b71f273f7c3783efbc8b8e4845f71fec290
Iraj Mashar <i.mashar@uniccomp.de>
2023-10-24 17:53:48 +0200

    added service

---
+++ src/app/common/appconfig.ts
@@ -0,0 +1,20 @@
+export const Appconfig = {
+    "api-key": "RGAPI-815128bc-9bf5-4bcc-8f20-317fcc1387fd",
+    "region": {
+        "North America":"",
+        "Europe West":"",
+        "Europe Nordic & East":"",
+        "Oceania":"",
+        "Korea":"",
+        "Japan":"",
+        "Brazil":"",
+        "LAS":"",
+        "LAN":"",
+        "Russia":"",
+        "Turkiye":"",
+        "Philippines":"",
+        "Taiwan":"",
+        "Vietnam":"",
+        "Thailand":""
+    }
+}
